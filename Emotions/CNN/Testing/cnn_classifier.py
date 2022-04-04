211acc3547e2e0bc673888a311a6832004a848f6
HarishRam10 <54903276+HarishRam10@users.noreply.github.com>
2022-04-04 12:51:56 -0400

    Update cnn_classifier.py

--- Emotions/CNN/Testing/cnn_classifier.py
+++ Emotions/CNN/Testing/cnn_classifier.py
@@ -1,7 +1,7 @@
 import torch
 import sys
 sys.path.insert(1, '/home/ubuntu/capstone/CNN')
-from Models.cnn import CNN, train_and_test, evalute_best_model, add_linear
+from Models.cnn import CNN, train_and_test, evaluate_best_model, add_linear
 from Models.autoencoder import cal, Encoder, Decoder, Classifier
 from Utility.dataloader import dataloader
 from Utility.utility import manual_label_encoder
@@ -29,28 +29,24 @@ train_loader = dataloader(train_df, OUTPUTS_a = 6, BATCH_SIZE = 64, IMAGE_SIZE=1
 test_loader = dataloader(test_df, OUTPUTS_a = 6, BATCH_SIZE = 64, IMAGE_SIZE=128)
 
 batch_size = 64
-epochs = 5
+epochs = 25
 IMAGE_SIZE = 128
 num_layers = 3
 learning_rate = 0.00002
 d = 64
 
 classes = ['ANG', 'DIS', 'FEA', 'HAP', 'NEU', 'SAD']
-#classes = list(train_df.label.unique)
 OUTPUTS_a = len(classes)
 
 jj, kk = cal(IMAGE_SIZE, num_layers)
 encoder = Encoder(encoded_space_dim=d, jj=jj, kk=kk).to(device)
-decoder = Decoder(encoded_space_dim=d, jj=jj, kk=kk).to(device)
-
-#cnn = CNN(encoded_space_dim=batch_size, jj=jj, kk=kk)
-cnn = Classifier(encoder)
+#decoder = Decoder(encoded_space_dim=d, jj=jj, kk=kk).to(device)
 
 PATH_SAVE = "/home/ubuntu/capstone/CNN/Models/Saved_Models/"
-cnn.load_state_dict(torch.load(PATH_SAVE + 'encoder_{}_layers.pt'.format(num_layers)))
 
-#cnn = add_linear(cnn, OUTPUTS_a)
-#cnn = nn.Sequential(CNN(True), nn.Linear(64, OUTPUTS_a))
+encoder.load_state_dict(torch.load(PATH_SAVE + "encoder_{}_layers.pt".format(num_layers)))
+cnn = Classifier(encoder, d, OUTPUTS_a)
 
-train_and_test(cnn, train_loader, test_loader, classes, num_layers, epochs, batch_size, learning_rate)
-evalute_best_model(cnn, test_loader, classes, num_layers)
+model_name = 'cnn_classifier_{}_layers.pt'.format(num_layers)
+train_and_test(cnn, train_loader, test_loader, classes, model_name, epochs, batch_size, learning_rate)
+evaluate_best_model(cnn, test_loader, classes, model_name)
