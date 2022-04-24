7d202b93ce0265d844b1dbfbadf43427fcd9b5d5
HarishRam10 <54903276+HarishRam10@users.noreply.github.com>
2022-04-24 15:58:40 -0400

    Update train_cnn.py

--- Emotions/CNN/Training/train_cnn.py
+++ Emotions/CNN/Training/train_cnn.py
@@ -2,53 +2,70 @@ import torch
 import torch.nn as nn
 import sys
 sys.path.insert(1, '/home/ubuntu/capstone/CNN')
-from Models.cnn import CNN, train_and_test, evalute_best_model, add_linear
+from Models.cnn import CNN, train_and_test, evaluate_best_model, add_linear, pretrained_model, CNN9
 from Models.autoencoder import cal
 from Utility.dataloader import dataloader
-from Utility.utility import manual_label_encoder
-import pandas as pd
+from Utility.utility import manual_label_encoder, get_model_params, compute_metrics, get_classes
+import argparse
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-#emotion
-csv_path = '/home/ubuntu/capstone/Data/'
-train_df = pd.read_csv(csv_path + "emotion_train.csv")
-train_df = train_df[['emotion', "Image_file_path"]]
-train_df.columns=['label','id']
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-train_df['label'] = manual_label_encoder(train_df['label'],'emotion')
+    parser.add_argument("--csv_load_path", default=None, type=str, required=True)
+    parser.add_argument('--category', default=None, type=str, required=True) #category (Ex. emotion, race, etc.)
 
+    args = parser.parse_args()
+    category = args.category
 
-test_df = pd.read_csv(csv_path + "emotion_test.csv")
-test_df = test_df[['emotion', "Image_file_path"]]
-test_df.columns=['label','id']
+    parser.add_argument("--train_csv", default=f"{category}_train.csv", type=str, required=False)  # train_csv
+    parser.add_argument("--val_csv", default=f"{category}_val.csv", type=str, required=False)  # val_csv
+    parser.add_argument("--test_csv", default=f"{category}_test.csv", type=str, required=False)  # test_csv
 
-test_df['label'] = manual_label_encoder(test_df['label'],'emotion')
+    parser.add_argument("--epochs", default=30, type=int, required=False)
+    parser.add_argument("--batch_size", default=64, type=int, required=False)
+    parser.add_argument("--learning_rate", default=1e-3, type=int, required=False)
 
+    parser.add_argument("--model",default=None, type=str, required=True)
+    parser.add_argument("--model_save_path", default=None, type=str, required=True)
 
-#race
-# train_df = pd.read_csv("race_train.csv")
-# test_df = pd.read_csv("race_test.csv")
+    epochs = args.epochs
+    batch_size = args.batch_size
+    learning_rate = args.learning_rate
+    train_df = args.train_csv
+    val_df = args.val_csv
+    test_df = args.test_csv
+    model = args.model
+    model_save_path = args.model_save_path
 
-classes = ['ANG', 'DIS', 'FEA', 'HAP', 'NEU', 'SAD']
+    train_df = train_df[[category, "Image_file_path"]]
+    train_df.columns=['label','id']
+    train_df['label'] = manual_label_encoder(train_df['label'],category)
 
-batch_size = 64
-epochs = 5
-IMAGE_SIZE = 128
-num_layers = 3
-learning_rate = 0.00002
-OUTPUTS_a = len(classes)
+    val_df = val_df[[category, "Image_file_path"]]
+    val_df.columns=['label','id']
+    val_df['label'] = manual_label_encoder(val_df['label'],category)
 
-train_loader = dataloader(train_df, OUTPUTS_a = 6, BATCH_SIZE = 64, IMAGE_SIZE=128)
-test_loader = dataloader(test_df, OUTPUTS_a = 6, BATCH_SIZE = 64, IMAGE_SIZE=128)
+    test_df = test_df[[category, "Image_file_path"]]
+    test_df.columns=['label','id']
+    test_df['label'] = manual_label_encoder(test_df['label'],category)
 
-jj, kk = cal(IMAGE_SIZE, num_layers)
-cnn = CNN(encoded_space_dim=batch_size, jj=jj, kk=kk)
-cnn = add_linear(cnn, OUTPUTS_a)
+    classes = get_classes(category)
+    OUTPUTS_a = len(classes)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+    d = 64
+    IMAGE_SIZE = 128
 
-train_and_test(cnn, train_loader, test_loader, classes, num_layers, epochs, batch_size, learning_rate)
+    train_loader = dataloader(train_df, OUTPUTS_a = OUTPUTS_a, BATCH_SIZE = batch_size, IMAGE_SIZE=IMAGE_SIZE)
+    val_loader = dataloader(val_df, OUTPUTS_a = OUTPUTS_a, BATCH_SIZE = batch_size, IMAGE_SIZE=IMAGE_SIZE)
+    test_loader = dataloader(test_df, OUTPUTS_a = OUTPUTS_a, BATCH_SIZE = batch_size, IMAGE_SIZE=IMAGE_SIZE)
 
-evalute_best_model(cnn, test_loader, classes, num_layers)
+    cnn = pretrained_model(model).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+
+    model_name = f'{model}_{epochs}_{batch_size}_{learning_rate}.pt'
+    train_and_test(cnn, train_loader, val_loader, classes, model_name, model_save_path, epochs, batch_size, learning_rate)
+    evaluate_best_model(cnn, test_loader, classes, model_name, model_save_path)
