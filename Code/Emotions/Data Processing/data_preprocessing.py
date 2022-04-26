850200dfda179ad70ae633c7c18c28563392d8d3
HarishRam10 <54903276+HarishRam10@users.noreply.github.com>
2022-04-26 18:28:36 -0400

    Update data_preprocessing.py

--- Code/Emotions/Data Processing/data_preprocessing.py
+++ Code/Emotions/Data Processing/data_preprocessing.py
@@ -20,24 +20,24 @@ def train_val_test_split(df, category, csv_save_path): #split into 72% train, 8%
 
 def generate_csv(csv_file, csv_path, audio_path, category):
     df1 = pd.read_csv(csv_path + csv_file)
-    df1 = df1[df1["Race"] != "Unknown"]
+    df1 = df1[df1["race"] != "Unknown"]
     df1['Audio_file_name'] = df1['File Name']
     df1['Audio_file_path'] = audio_path + df1['Audio_file_name']
     df = df1[[category, "Audio_file_name", "Audio_file_path"]]
 
-    df.to_csv(csv_path + "CREMA_with_audio_path.csv")
-
+    df.to_csv(csv_path + f"{category}_with_audio_path.csv")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_file", default="CREMA_no_path.csv", type=str, required=True)  # Path of csv to load
+    parser.add_argument("--csv_file", default="CREMA_no_path.csv", type=str, required=True)  # CREMA_no_path.csv
     parser.add_argument("--csv_path", default=None, type=str, required=True)  # Path to save the csv file
-    parser.add_argument('--audio_path',default=None,type =str, required=True) # csv file name to save
-    parser.add_argument('--category', default=None, type=str, required=True) #category (Ex. emotion, race, etc.)
+    parser.add_argument('--audio_path',default=None,type =str, required=True) # path of AudioWAV folder
+    parser.add_argument('--category', default=None, type=str, required=True) # category (Ex. emotion, race, etc.)
     args = parser.parse_args()
     csv_file = args.csv_file
     csv_path = args.csv_path
     audio_path = args.audio_path
     category = args.category
 
-    generate_csv(csv_file, csv_path, audio_path, category) #will generate csv file with audio name and audio path
+    generate_csv(csv_file, csv_path, audio_path, category)
+    #will generate csv file with 3 columns: label, audio file name and audio file path
