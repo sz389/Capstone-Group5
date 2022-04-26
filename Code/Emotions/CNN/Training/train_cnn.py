6ba6e7f502a00ae5f8374f5cb228740c036605ec
HarishRam10 <54903276+HarishRam10@users.noreply.github.com>
2022-04-26 17:27:28 -0400

    Update train_cnn.py

--- Code/Emotions/CNN/Training/train_cnn.py
+++ Code/Emotions/CNN/Training/train_cnn.py
@@ -18,28 +18,24 @@ if __name__ == '__main__':
     parser.add_argument('--category', default=None, type=str, required=True) #category (Ex. emotion, race, etc.)
     parser.add_argument("--model",default=None, type=str, required=True)
     parser.add_argument("--model_save_path", default=None, type=str, required=True)
-
-    args = parser.parse_args()
-    category = args.category
-
-    parser.add_argument("--train_csv", default=f"{category}_train.csv", type=str, required=False)  # train_csv
-    parser.add_argument("--val_csv", default=f"{category}_val.csv", type=str, required=False)  # val_csv
-    parser.add_argument("--test_csv", default=f"{category}_test.csv", type=str, required=False)  # test_csv
-
-    parser.add_argument("--epochs", default=30, type=int, required=False)
+    parser.add_argument("--epochs", default=30, type=int, required=True)
     parser.add_argument("--batch_size", default=64, type=int, required=False)
     parser.add_argument("--learning_rate", default=1e-3, type=int, required=False)
+    parser.add_argument("--train_csv", default=f"emotion_train.csv", type=str, required=False)  # train_csv
+    parser.add_argument("--val_csv", default=f"emotion_val.csv", type=str, required=False)  # val_csv
+    parser.add_argument("--test_csv", default=f"emotion_test.csv", type=str, required=False)  # test_csv
 
     args = parser.parse_args()
-    epochs = args.epochs
     csv_load_path = args.csv_load_path
+    category = args.category
+    model = args.model
+    model_save_path = args.model_save_path
+    epochs = args.epochs
     batch_size = args.batch_size
     learning_rate = args.learning_rate
     train_csv = args.train_csv
     val_csv = args.val_csv
     test_csv = args.test_csv
-    model = args.model
-    model_save_path = args.model_save_path
 
     train_df = pd.read_csv(csv_load_path + train_csv)
     train_df = train_df[[category, "Image_file_path"]]
@@ -72,5 +68,10 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
 
     model_name = f'{model}_{epochs}_{batch_size}_{learning_rate}.pt'
+
+    print("Training...")
     train_and_test(cnn, train_loader, val_loader, classes, model_name, model_save_path, epochs, batch_size, learning_rate)
+    print()
+    print("Evaluating...")
+    print()
     evaluate_best_model(cnn, test_loader, classes, model_name, model_save_path)
