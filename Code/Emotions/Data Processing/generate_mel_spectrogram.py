ba56b06a0ee3f075d1289b2399bf698d5175daf7
HarishRam10 <54903276+HarishRam10@users.noreply.github.com>
2022-04-26 18:29:22 -0400

    Update generate_mel_spectrogram.py

--- Code/Emotions/Data Processing/generate_mel_spectrogram.py
+++ Code/Emotions/Data Processing/generate_mel_spectrogram.py
@@ -11,14 +11,15 @@ import tqdm
 import soundfile as sf
 import argparse
 import sys
-sys.path.insert(1, '/home/ubuntu/capstone/CNN')
-from Utility.utility import manual_label_encoder, get_model_params, compute_metrics, get_filename
+sys.path.insert(1, '/home/ubuntu/capstone/Data_Processing')
+from data_preprocessing import train_val_test_split
+
 #%%
 def melspec_librosa(x,sample_rate = 16000,
-    n_fft = 1024,
+    n_fft = 1536,
     win_length = None,
     hop_length = 512,
-    n_mels = 180):
+    n_mels = 160):
     return librosa.feature.melspectrogram(
     y = x,
     sr=sample_rate,
@@ -45,11 +46,10 @@ def save_spectrogram(spec,file,path, aspect='auto', xmax=None):
       path+'/' + file + ".jpg",
       bbox_inches='tight', pad_inches=0)
   return path+'/'+file + ".jpg"
-def generate_spec(path_column,file_name_column,label_column,save_path):
+def generate_spec(path_column,file_name_column,save_path):
     # wav_name_list =[]
     # origin_list = []
     img_name_list=[]
-    label_list=[]
     # labelnum_list = []
     for i in range(len(path_column)):
         audio, sr = librosa.load(path_column[i],sr=16000)
@@ -61,10 +61,10 @@ def generate_spec(path_column,file_name_column,label_column,save_path):
         # sf.write(out_filename, audio, sr)
         img_name=save_spectrogram(melspec_librosa(audio,n_mels=180),file_name_column[i],save_path+'/mel_spectrograms/')
         img_name_list.append(img_name)
+
         # origin_list.append(origin[i])
         # labelnum_list.append(label_num[i])
-        label_list.append(label_column[i])
-    return img_name_list,label_list
+    return img_name_list
 
 def save_spectrogram_gui(spec,file,path, aspect='auto', xmax=None):
   fig, axs = plt.subplots(1, 1)
@@ -83,19 +83,20 @@ def save_spectrogram_gui(spec,file,path, aspect='auto', xmax=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--csv_path", default=None, type=str, required=True)  # Path of csv to load
-    parser.add_argument("--save_path", default=None, type=str, required=True)  # Path to save the csv file
-    parser.add_argument('--save_csv',default=None,type = str, required=True) # csv file name to save
+    parser.add_argument("--load_csv_path", default=None, type=str, required=True)  # Path of csv to load
+    parser.add_argument("--save_spec_path", default=None, type=str, required=True)  # path to store spectrograms
+    parser.add_argument("--save_csv_path", default=None, type=str, required=True) #path to save csv
+    parser.add_argument("--category", default=None, type=str, required=True)  # #category (Ex. emotion, race, etc.)
     args = parser.parse_args()
-    csv_path = args.csv_path
-    save_path = args.save_path
-    csv_name = args.save_csv
-    if not os.path.exists(save_path+'/mel_spectrograms'):
-        os.makedirs(save_path+'/mel_spectrograms')
-    df = pd.read_csv(csv_path)
-    filename = get_filename(df['id'])
-    img,label = generate_spec(df['id'],filename,df['label'],save_path)
-    df12 = pd.DataFrame()
-    df12['id'] = img
-    df12['label'] = label
-    df12.to_csv(save_path+'/'+csv_name,index=False)
+    load_csv_path = args.load_csv_path
+    save_spec_path = args.save_spec_path
+    save_csv_path = args.save_csv_path
+    category = args.category
+    if not os.path.exists(save_spec_path+'/mel_spectrograms'):
+        os.makedirs(save_spec_path+'/mel_spectrograms')
+
+    df = pd.read_csv(load_csv_path + f"{category}_with_audio_path.csv")
+    img = generate_spec(df['Audio_file_path'],df['Audio_file_name'], save_spec_path) #img = list of file paths
+    df['Image_file_path'] = img
+
+    train_val_test_split(df, category, save_csv_path) #saves train, validation and test csv file
