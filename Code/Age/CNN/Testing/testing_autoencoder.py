8e973278cffd9d1f998da0acf850a588565da6d0
HarishRam10 <54903276+HarishRam10@users.noreply.github.com>
2022-04-26 23:02:04 -0400

    Update testing_autoencoder.py

--- Code/Age/CNN/Testing/testing_autoencoder.py
+++ Code/Age/CNN/Testing/testing_autoencoder.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     parser.add_argument("--csv_load_path", default=None, type=str, required=True)
     parser.add_argument('--category', default=None, type=str, required=True) #category (Ex. emotion, race, etc.)
     parser.add_argument("--model_load_path", default=None, type=str, required=True)
-    parser.add_argument("--train_csv", default=f"emotion_train.csv", type=str, required=False)  # train_csv
+    parser.add_argument("--train_csv", default=f"age_train.csv", type=str, required=False)  # train_csv
     parser.add_argument("--epochs", default=5, type=int, required=False)
     parser.add_argument("--batch_size", default=64, type=int, required=False)
     parser.add_argument("--learning_rate", default=1e-3, type=int, required=False)
