480e4bec6d4ddf45588a9299346eedfb35e9fe23
Jiachen Sands <64928565+WiseBubu@users.noreply.github.com>
2022-04-28 00:12:29 -0400

    Update Combine_Origin_Augmentated.py

--- Code/Accent/Data Preprocessing/Combine_Origin_Augmentated.py
+++ Code/Accent/Data Preprocessing/Combine_Origin_Augmentated.py
@@ -1,56 +1,5 @@
 #%%
 import pandas as pd
-# csv_path = '/home/ubuntu/Capstone/'
-# df_augmented = pd.read_csv(csv_path + 'Augmented_Image_data_2.csv')
-# df_augmented.head(10)
-#
-# #%%
-# df_augmented.groupby('label').count()
-#
-# #%%
-# # original data - entire set
-# df = pd.read_csv(csv_path + 'split_audio_img.csv')
-#
-# #%%
-# df.groupby('label').count()
-#
-# #%%
-# # remove test set out of CapstoneZZ
-# df_testimg = pd.read_csv(csv_path+"Test_Images.csv")
-# df_testimg.head()
-# test_img_list = df_testimg['img_name']
-# df2 = df[~df['img_name'].isin(test_img_list)]
-# # df2 is the original split images for cnn models (exclude testing data)
-#
-# #%%
-# df2.groupby('label').count()
-# #%%
-# # Concatenate original files and augmented files
-# df_augmented['Path'] = '/home/ubuntu/Capstone/Augmented_MelSpectrogram_2/' + df_augmented['Filename']
-# df_augmented = df_augmented.loc[:, ~df_augmented.columns.str.contains('^Unnamed')]
-# #%%
-# # add a path in original dataset
-# df2['Path'] = '/home/ubuntu/Capstone/split_audio_img/' + df2['img_name']
-# # rename columns in df2
-# df2.rename({'img_name': 'Filename'}, axis=1, inplace=True)
-# #%%
-# # combine original data and augmented data
-# df_original_augmented = pd.concat([df2,df_augmented],ignore_index=True, sort=False )
-# #%%
-# # export csv of df_original_augmented
-# df_original_augmented.to_csv(csv_path + 'Origin_Augmented_Training_Data_2.csv')
-# #%%
-# df_original_augmented.groupby(['label']).count()
-# #%%
-# import pandas as pd
-# aug_path = '/home/ubuntu/Capstone/data_csv_0326/augmented_csv/'
-# ori_path = '/home/ubuntu/Capstone/data_csv_0326/'
-#
-# aug_df = pd.read_csv( aug_path + 'Augmented_Images.csv')
-# ori_df = pd.read_csv(ori_path + 'data_csv_0326accent_train_trim_split.csv')
-# #%%
-# ori_df[id] = '/home/ubuntu/Capstone/data_csv_0326/mel_spectrograms/' + ori_df['img_name']
-# aug_df[id] = '/home/ubuntu/Capstone/data_csv_0326/augmented_csv/augmented_images/' + aug_df['Filename']
 
 #%%
 import argparse
